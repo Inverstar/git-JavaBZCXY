26a96442c4843c5f4258545118787644c5b92e06
Inverstar <lnq1208@163.com>
2021-08-01 14:49:39 +0800

    Update vcs.xml
    出现编码问题, 回滚解决

---
+++ src/Day1/Demo1.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class Demo1{
+	public static void main (String args[]){
+		System.out.println("我叫 旋涡刘能");
+		System.out.println("今年50岁");
+		System.out.println("家住象牙山村");
+		System.out.println("我的亲家叫做宇智波赵四儿");
+		System.out.println("1+1");
+		System.out.println(1/0);
+	}
+}
---
+++ src/Day1/Demo2.java
@@ -0,0 +1,16 @@
+// 标志在代码中的目录名 >>> 包名
+// 受保护的资源文件夹 
+// 区分多个不同目录下同名的文件
+package Day1;
+            //类名
+public class Demo2{
+					  //方法名
+	public static void main (String args[]){
+		// 变量名
+		int age =10;
+		System.out.println(age);
+	}
+					// 方法名   a b 参数名
+	public static void add(int a ,int b){
+	}
+}
