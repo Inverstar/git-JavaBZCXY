14f14733a72fc2d0015fa336285404467fc71c77
Inverstar <lnq1208@163.com>
2021-08-01 13:16:39 +0800

    初始化

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import 数据.基本数据类型;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        基本数据类型 a1 = new 基本数据类型();
+        a1.输出();
     }
 }
